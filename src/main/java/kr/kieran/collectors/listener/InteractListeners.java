39ef61e65d1606724dc7e183a45d0feab7d210b0
Kieraaaan <ramseykieran@gmail.com>
2021-04-13 17:46:33 +0100

    Add collector sell logic and prepare for collector gui integration

--- src/main/java/kr/kieran/collectors/listener/InteractListeners.java
+++ src/main/java/kr/kieran/collectors/listener/InteractListeners.java
@@ -1,9 +1,18 @@
 package kr.kieran.collectors.listener;
 
 import kr.kieran.collectors.CollectorsPlugin;
+import kr.kieran.collectors.model.Collector;
+import kr.kieran.collectors.util.Color;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.Map;
 
 public class InteractListeners implements Listener
 {
@@ -15,10 +24,88 @@ public class InteractListeners implements Listener
         this.plugin = plugin;
     }
 
-    // LISTENER: COLLECTOR INTERACT
-    @EventHandler(ignoreCancelled = true)
-    public void collectorInteract(PlayerInteractEvent event)
+    // LISTENER: COLLECTOR OPEN
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void open(PlayerInteractEvent event)
     {
+        // Args
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+        if (block == null) return;
+        long chunkId = block.getChunk().getChunkKey();
+
+        // Check
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        // TODO: Fix check to prevent cancelling placing regular blocks
+        if (!plugin.getChunkManager().canUseChunk(chunkId))
+        {
+            player.sendMessage(Color.color(plugin.getConfig().getString("messages.chunk-locked")));
+            event.setCancelled(true);
+            return;
+        }
+
+        // Collector
+        Collector collector = plugin.getCollectorManager().getByLocation(block.getLocation());
+        if (collector == null) return;
+
+        // Cancel
+        event.setCancelled(true);
+
+        // TODO: Open the collector menu
+    }
+
+    // LISTENER: COLLECTOR SELL
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void sell(PlayerInteractEvent event)
+    {
+        // Args
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+        if (block == null) return;
+        long chunkId = block.getChunk().getChunkKey();
+
+        // Check
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (!player.isSneaking()) return;
+        // TODO: Fix check to prevent cancelling placing regular blocks
+        if (!plugin.getChunkManager().canUseChunk(chunkId))
+        {
+            player.sendMessage(Color.color(plugin.getConfig().getString("messages.chunk-locked")));
+            event.setCancelled(true);
+            return;
+        }
+
+        // Collector
+        Collector collector = plugin.getCollectorManager().getByLocation(block.getLocation());
+        if (collector == null) return;
+
+        // Cancel
+        event.setCancelled(true);
+
+        // Sell collector contents
+        double total = 0.0d, finalTotal;
+        for (Map.Entry<Material, Integer> entry : collector.getContents().entrySet())
+        {
+            Material material = entry.getKey();
+            int amount = entry.getValue();
+            if (amount <= 0) continue;
+            if (!plugin.getConfig().isSet("prices." + material.name())) continue;
+
+            double price = plugin.getConfig().getDouble("prices." + material.name());
+            total += price * amount;
+        }
+        finalTotal = total;
+
+        // Clear
+        collector.clearContents();
+
+        // Save & Inform
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            plugin.getCollectorManager().save(collector, after -> {
+                // TODO: Give the player money using Vault
+                player.sendMessage(Color.color(plugin.getConfig().getString("messages.contents-sold").replace("%total%", String.format("%,.2f", finalTotal))));
+            });
+        });
     }
 
 }
