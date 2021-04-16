ecd872d94cc9a37958577a93f6fd596ab641d047
Kieraaaan <ramseykieran@gmail.com>
2021-04-16 12:12:22 +0100

    Add null check for placing/destroying collectors

--- src/main/java/kr/kieran/collectors/listener/PlacementListeners.java
+++ src/main/java/kr/kieran/collectors/listener/PlacementListeners.java
@@ -62,8 +62,18 @@ public class PlacementListeners implements Listener
 
         // Create & Unlock
         plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
-            plugin.getCollectorManager().create(chunkId, SerializationUtil.serialize(location),after -> {
+            plugin.getCollectorManager().create(chunkId, SerializationUtil.serialize(location), after -> {
+                // Unlock
                 plugin.getChunkManager().unlock(chunkId);
+
+                // Check
+                if (after == null)
+                {
+                    player.sendMessage(Color.color(plugin.getConfig().getString("messages.creation-fail")));
+                    return;
+                }
+
+                // Inform
                 player.sendMessage(Color.color(plugin.getConfig().getString("messages.placed-collector").replace("%x%", String.format("%,d", chunk.getX())).replace("%z%", String.format("%,d", chunk.getZ()))));
             });
         });
@@ -98,7 +108,17 @@ public class PlacementListeners implements Listener
         // Destroy & Unlock
         plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
             plugin.getCollectorManager().delete(collector, after -> {
+                // Unlock
                 plugin.getChunkManager().unlock(chunkId);
+
+                // Check
+                if (after == null)
+                {
+                    player.sendMessage(Color.color(plugin.getConfig().getString("messages.deletion-fail")));
+                    return;
+                }
+
+                // Inform
                 player.sendMessage(Color.color(plugin.getConfig().getString("messages.destroyed-collector")));
             });
         });
